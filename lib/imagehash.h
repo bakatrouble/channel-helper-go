10c4d077ba541848cae0bcbd40f810328e681d30
bakatrouble <bakatrouble@gmail.com>
2025-08-09 02:07:01 +0300

    add rust library

---
+++ lib/imagehash.h
@@ -0,0 +1,2 @@
+#include <stdint.h>
+char *hash_image(const uint8_t *buf, uint32_t size);
---
+++ lib/imagehash/src/lib.rs
@@ -0,0 +1,10 @@
+#[unsafe(no_mangle)]
+pub extern "C" fn hash_image(buf: *const u8, size: u32) -> *mut std::ffi::c_char {
+    let image_bytes = unsafe { std::slice::from_raw_parts(buf, size as usize) };
+    let img = image::load_from_memory(image_bytes)
+        .expect("Failed to load image from bytes");
+    let hasher = imagehash::PerceptualHash::new()
+        .with_image_size(16, 16)
+        .with_hash_size(16, 16);
+    std::ffi::CString::new(hasher.hash(&img).to_string()).unwrap().into_raw()
+}

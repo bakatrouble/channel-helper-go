f80e32936ed38b8a5f0a5c77c2f16f02f2620199
bakatrouble <bakatrouble@gmail.com>
2025-08-10 17:36:30 +0300

    Update hashing lib

--- lib/imagehash/src/lib.rs
+++ lib/imagehash/src/lib.rs
@@ -4,7 +4,7 @@ pub extern "C" fn hash_image(buf: *const u8, size: u32) -> *mut std::ffi::c_char
     let img = image::load_from_memory(image_bytes)
         .expect("Failed to load image from bytes");
     let hasher = imagehash::PerceptualHash::new()
-        .with_image_size(16, 16)
-        .with_hash_size(16, 16);
+        .with_image_size(8, 8)
+        .with_hash_size(8, 8);
     std::ffi::CString::new(hasher.hash(&img).to_string()).unwrap().into_raw()
 }
